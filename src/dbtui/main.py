b013c56e3f373df6d292c62a06c500b92b5acce2
Mike <ayamachi@protonmail.com>
2025-10-28 19:44:34 +0100

    base

---
+++ src/dbtui/main.py
@@ -0,0 +1,6 @@
+import os 
+from project import DbtProject
+
+target_dbt_project = DbtProject(os.environ['PROJECT'])
+
+
---
+++ src/dbtui/project.py
@@ -0,0 +1,33 @@
+import os 
+from os.path import join as opj
+import yaml
+from typing import Any, Generator
+
+
+class DbtModel:
+    pass
+
+
+class DbtProject:
+    root_folder: str 
+    dbt_project_yml: Generator
+    model_folder: str
+    models: list[DbtModel]
+
+    def parse_dbt_project(self, dbt_project_raw: str) -> None:
+        self.dbt_project_yml = yaml.parse(dbt_project_raw)
+        print(self.dbt_project_yml)
+        pass
+
+    def __init__(self, root_folder) -> None:
+        if not os.path.exists(root_folder):
+            raise FileNotFoundError("Folder not found: %s", root_folder)
+        self.root_folder = root_folder
+        try:
+            with open(opj(self.root_folder, 'dbt_project.yml'), 'r', encoding='utf-8') as f:
+                self.parse_dbt_project(f.read())
+        except FileNotFoundError:
+            raise FileNotFoundError("dbt folder is present, but dbt_project.yml is not found: %s", root_folder)
+        
+        
+
