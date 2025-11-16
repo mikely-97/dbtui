fa53361ecaefa0789667fa1248d1b9c32666ddd3
Mike <ayamachi@protonmail.com>
2025-11-16 20:00:16 +0100

    refactor

---
+++ src/frontend/common.py
@@ -0,0 +1,37 @@
+from typing import Iterable
+
+from textual import widgets, containers
+
+isolated = True
+if isolated:
+    from pseudo import DbtProject, DbtModel
+else:
+    from ..dbtui.project import DbtProject, DbtModel
+
+class ModelListItem(widgets.ListItem):
+
+    dbt_model: DbtModel
+
+    def __init__(self, model: DbtModel, **kwargs):
+
+        super().__init__(
+            containers.VerticalGroup(
+                widgets.Label(model.name),
+                widgets.Rule(),
+                widgets.Label(model.filepath),
+            ),
+            **kwargs
+        )
+        self.dbt_model = model
+    
+
+class ModelList(widgets.ListView):
+
+    def populate_with_models(self, models: Iterable[DbtModel]):
+        self.clear()
+        for model in models:
+            self.append(
+                ModelListItem(
+                    model=model
+                )
+            )
---
+++ src/frontend/model_view.py
@@ -0,0 +1,86 @@
+from typing import Literal
+
+from textual import widgets, containers, screen
+
+from common import ModelList, ModelListItem
+
+isolated = True
+if isolated:
+    from pseudo import DbtProject, DbtModel
+else:
+    from ..dbtui.project import DbtProject, DbtModel
+
+
+class ModelRelativesList(ModelList):
+    
+    def on_list_view_highlighted(self, event: widgets.ListView.Highlighted) -> None:
+        list_item: ModelListItem = event.item
+    
+    def on_model_change(self, relatives_type: Literal['parents', 'children']):
+        model: DbtModel = self.app.ctx['active_model']
+        if relatives_type == 'parents':
+            self.populate_with_models(model.parents())
+        elif relatives_type == 'children':
+            self.populate_with_models(model.children())
+        else:
+            raise NotImplementedError
+
+
+class ModelView(screen.Screen):
+
+
+    BINDINGS = [
+        # ("O", "options", "open options"),
+        ("f", "push_screen('model_search')", "search models"),
+    ]
+
+    def compose(self):
+        yield containers.ScrollableContainer(
+            widgets.TextArea(
+                id='model_content',
+                name='content',
+                read_only=True, # TODO: not read-only when pressing Enter, back to read-only when pressing Esc 
+                show_line_numbers=True,
+                language='sql',
+            ),
+        )
+        yield containers.Horizontal(
+            ModelRelativesList(
+                id='parents',
+                name='model_parents',
+            ),
+            widgets.TextArea(
+                id='model_properties',
+                name='model properties',
+                read_only=True,
+            ),
+            ModelRelativesList(
+                id='children',
+                name='model children',
+            )
+        )
+    
+    def on_model_change(self):
+        model: DbtModel = self.app.ctx['active_model']
+        # model_content
+        model_content = self.get_widget_by_id('model_content')
+        assert isinstance(model_content, widgets.TextArea)
+        model_content.clear()
+        model_content.text = model.text
+        # parents
+        parents = self.get_widget_by_id('parents')
+        assert isinstance(parents, ModelRelativesList)
+        parents.on_model_change('parents')
+        # children
+        children = self.get_widget_by_id('children')
+        assert isinstance(children, ModelRelativesList)
+        children.on_model_change('children')
+        # TODO: model properties
+        pass
+
+    def on_mount(self):
+        if self.app.ctx['active_model'] is None:
+            self.app.push_screen('model_search')
+        assert isinstance(self.app.ctx['active_model'], DbtModel)
+        self.on_model_change()   
+
